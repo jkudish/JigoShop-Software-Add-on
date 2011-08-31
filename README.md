d4881532317c07e95d10660f333e07bd55500cab
Joachim Kudish <tech@jkudish.com>
2011-08-30 19:37:07 -0700

    second run at the auto-updater, not done yet but need the commit to be able to test further first run at the auto-updater, not done yet but need the commit to be able to test further
    don't update a live site with this update :)

--- README.md
+++ README.md
@@ -28,4 +28,4 @@ Current Version
 
 The line below is used for the updater API, please leave it untouched unless bumping the version up :)
 
-~Current Version: 1.2
+~Current Version: 1.2~
--- inc/_updater.php
+++ inc/_updater.php
@@ -1,12 +1,5 @@
 <?php
 
-if (!function_exists('add_action')) {
-	// WP hasn't run yet
-	
-	die('yo');
-	
-}
-
 // [debug]: Enable update check on every request. Normally you don't need this! This is for testing only!
 set_site_transient('update_plugins', null);
 
@@ -23,21 +16,11 @@ add_filter('pre_set_site_transient_update_plugins', 'check_for_plugin_update');
 
 function check_for_plugin_update($checked_data) {
 	
-	$api_url = 'http://jkudish.com/api/plugins/';
-	$plugin_slug = 'jigoshop-software';
-	
-	if (empty($checked_data->checked))
-		return $checked_data;
-	
-	$request_args = array(
-		'slug' => $plugin_slug,
-		'version' => $checked_data->checked[$plugin_slug .'/'. $plugin_slug .'.php'],
-	);
-	
-	$request_string = prepare_request('basic_check', $request_args);
-	
 	// Start checking for an update
-	$raw_response = wp_remote_post($api_url, $request_string);
+	$raw_response = wp_remote_get('https://github.com/jkudish/JigoShop-Software-Add-on');
+	$__version = explode('~Current Version:', $raw_response['body']);
+	$_version = explode('~', $_version[1]);
+	die(var_dump($_version[0]));
 	
 	if (!is_wp_error($raw_response) && ($raw_response['response']['code'] == 200))
 		$response = unserialize($raw_response['body']);
